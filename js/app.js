4a15a5c743560d693adc5e1fd1e8f4443aefb6cb
Fridakhalo <aj050630@gmail.com>
2017-11-16 13:52:55 +0100

    fonction

---
+++ js/app.js
@@ -0,0 +1,28 @@
+console.log("Hello Word");
+
+function addition(a, b){
+	return a + b;
+};
+ 
+console.log(addition(5, 2));
+
+function sub(a, b){
+	return a - b;
+};
+
+console.log(sub(5, 2));
+function divice(a,b){
+	if (b ===  0){
+		return "Error"
+	}else{
+		return a / b;
+	};
+};
+
+console.log(divice(12, 4));
+
+function mult(a, b){
+	return a * b;
+};
+
+console.log(mult(54, 33));
